3bc09e2ccca656175a202661113cef6b38c127bd
ducconit <ducconit@gmail.com>
2021-08-02 19:59:45 +0700

    Khai báo các method sử dụng Facade

--- src/Facades/Setting.php
+++ src/Facades/Setting.php
@@ -2,10 +2,17 @@
 
 namespace Ducnt\Plugins\Setting\Facades;
 
+use Ducnt\Plugins\Setting\Supports\DriverBase;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see
+ * @method static DriverBase all()
+ * @method static DriverBase get($key, $default = null)
+ * @method static DriverBase set(array|string $key, $value = null)
+ * @method static DriverBase forget(string $key)
+ * @method static DriverBase flush()
+ * @method static DriverBase save()
+ * @see DriverBase
  */
 class Setting extends Facade
 {
